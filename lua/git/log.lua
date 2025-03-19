8db430ac197cf5b328c163669e6adc13486314c3
Eric Wong <wsdjeg@outlook.com>
2025-03-19 10:15:34 +0800

    use logger.nvim

--- lua/git/log.lua
+++ lua/git/log.lua
@@ -1,10 +1,34 @@
-local logger = require('spacevim.logger').derive('git')
+local M = {}
+local logger
+function M.info(msg)
+  if not logger then
+    pcall(function()
+      logger = require('logger').derive('flygrep')
+      logger.info(msg)
+    end)
+  else
+    logger.info(msg)
+  end
+end
+function M.warn(msg)
+  if not logger then
+    pcall(function()
+      logger = require('logger').derive('flygrep')
+      logger.warn(msg)
+    end)
+  else
+    logger.warn(msg)
+  end
+end
+function M.debug(msg)
+  if not logger then
+    pcall(function()
+      logger = require('logger').derive('flygrep')
+      logger.debug(msg)
+    end)
+  else
+    logger.debug(msg)
+  end
+end
 
-
-return {
-  info = logger.info,
-  warn = logger.warn,
-  debug = logger.debug
-
-}
-
+return M
